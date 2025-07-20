79de9f49c35e271b8e9768427e6f21259dc0ab00
Aayush-Mathe <aayushmathe6@gmail.com>
2025-07-20 17:04:56 +0530

    Created Pokemon Avatars

---
+++ Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+    
+  
+      <h1>Welcome to the Pokédex App by Aayush Mathe</h1>
+      <p>Explore the world of Pokémon with our comprehensive Pokédex</p>
+      <p> Click Pokédex or Search for more info... </p>
+    </div>
+  );
+};
+
+export default Home;
